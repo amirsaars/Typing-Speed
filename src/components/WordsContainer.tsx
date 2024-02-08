db2368c8f51b172cf17d66b83defe4f73815ea86
AmirSaeed AryanMehr <amiraryanmehr82@gmail.com>
2024-02-08 14:26:05 +0330

    foreground layer fixed

--- src/components/WordsContainer.tsx
+++ src/components/WordsContainer.tsx
@@ -1,7 +1,7 @@
 function WordsContainer({ children }: { children: React.ReactNode }) {
   return (
     <div
-      className="relative whitespace-pre-wrap text-3xl max-w-xl text-justify leading-relaxed break-all mt-3'
+      className="relative whitespace-pre-wrap text-3xl max-w-xl leading-relaxed break-all mt-3'
   "
     >
       {children}
